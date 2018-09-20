19593577ab6e9645ae41e315ca389905049a790d
vlad <Korovin.Va@phystech.edu>
2018-09-20 18:31:52 +0300

    user interface

--- split/main.c
+++ split/main.c
@@ -9,12 +9,15 @@ struct List {
 };
 
 struct List * strings_division(struct List * output, char * array_intput, char * special_symbols);
+void Destroy(struct List * line);
+struct List * New();
 
 int main() {
 	struct List * line = NULL;
 	char input_string[100] = {0};
 	char symbols[20] = {0};
 	int counter = -1;
+	fprintf(stderr, "Enter your string, use \\n to complete the line input:\n");
 	do {
 		counter++;
 		assert(counter < 100);
@@ -22,6 +25,7 @@ int main() {
 	} while( input_string[counter] != '\n');
 	input_string[counter] = '\0';
 	counter = -1;
+	fprintf(stderr, "Enter your tokens, use \\n to complete the line input:\n");
 	do {
 		counter++;
 		assert(counter < 20);
@@ -36,8 +40,7 @@ int main() {
 		printf("%s\n", line->line[counter]);
 	}
 	printf("The number of strings: %d\n", line->number);
-	free(line->line);
-	free(line);
+	Destroy(line);
 	return 0;
 }
 
@@ -46,8 +49,8 @@ struct List * strings_division(struct List * output, char * array, char * array_
 	int array_lenght = 0;
 	int counter_1 = 0;
 	int counter_2 = 0;
-	output = (struct List *)calloc(1, sizeof(struct List));
-	output->line = (char**)calloc(100, sizeof(char*));
+	output = New();
+	assert(output);
 	output->line[output->number] = array;
 	output->number++;
 	for(; array[counter_1] != '\0'; counter_1++) {
@@ -62,5 +65,17 @@ struct List * strings_division(struct List * output, char * array, char * array_
 		output->number++;
 		}
 	}
+	return output;
+}
+
+void Destroy(struct List * line) {
+	free(line->line);
+	free(line);
+}
+
+struct List * New() {
+	struct List * output = NULL;
+	output = (struct List *)calloc(1, sizeof(struct List));
+	output->line = (char**)calloc(100, sizeof(char*));
 	return output;
 }
