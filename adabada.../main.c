c8521fd21d3d71fe011c0c8a41a9df3efd1b6dca
Vlad <vlad.miryaha@gmail.com>
2018-09-26 23:59:01 +0300

    fixit

--- adabada.../main.c
+++ adabada.../main.c
@@ -3,6 +3,13 @@
 #include <string.h>
 #include <assert.h>
 
+// FIXIT: стилевые замечания
+// 1) пробелы вокруг бинарных операторов (n+1) -> (n + 1)
+// 2) нет пробелы перед запятой: (n , string) -> (n, string)
+// 3) именование переменных: counter_1, counter_2, ascii ... неясно их предназначение ... возможно хватит одного с названием, например
+// currentStringLength или currentStringHalfLength ... и вместо ascii, например, nextSymbol
+// Крутость кода в том числе измеряется тем, сколько надо времени новому человеку, чтобы с ним разобраться.
+
 void GenerateString(int n, char * s);
 
 int main()
@@ -30,6 +37,8 @@ void GenerateString(int n, char * s)
 		strncpy(s + counter_1 * 2, s, counter_2);
 		s[counter_2] = ascii;
 		counter_1 *= 2;
+		// Современные компиляторы в режиме release (компиляция с ключом -O2 или -O3 для gcc) сгенерит одинаковый ассемблерный код
+		// и для *2 и для << 1. Т.е. здесь лучше отдать предпочтение читаемости кода и выбрать вариант * 2.
 		counter_2 = (counter_2 << 1) + 1;
 		ascii += 1;
 	}
