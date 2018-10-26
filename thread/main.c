b5899700f2191db6c2e6471e29647afcbb8743c6
Vlad Korovin <KorovinVA@phystech.edu>
2018-10-26 16:31:08 +0300

    clock + dispersion

--- thread/main.c
+++ thread/main.c
@@ -7,40 +7,42 @@
 #include <ctype.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <time.h>
 
 #define AMOUNT_OF_DATA 100000000
 #undef RAND_MAX 
 #define RAND_MAX 50
-#define SEVERAL_THREADS 10
 #define NO_THREADS 0
 
 struct thread_info {
 	pthread_t th_mark; 
 	int *start;
 	int addend; 					//The quantity of numbers to count in a thread.
-	struct mean_variance *values;
+	struct return_data *values;
 };
 
-struct mean_variance {
+struct return_data {
 	double average;
 	double dispersion;
+	double time_spent;
 };
 
-struct mean_variance calculate(int number_of_threads, int *data);
+struct return_data calculate(int number_of_threads, int *data);
 void *thread_work(void * arg);
 void create_data(int * array);
 
 int main() {
+	int number_of_threads = 0;
 	int *data = calloc(AMOUNT_OF_DATA, sizeof(int));
-	struct mean_variance val_no_thr = {0, 0};
-	struct mean_variance val = {0, 0};
+	struct return_data val_no_thr = {0, 0};
+	struct return_data val = {0, 0};
+	printf("Enter a number of threads:\n");
+	scanf("%d", &number_of_threads);
 	create_data(data);
 	val_no_thr = calculate(NO_THREADS, data);
-	val = calculate(SEVERAL_THREADS, data);
-	printf("\nNo_threads\nAverage value: %f\nDispersion: %f\n", val_no_thr.average, val_no_thr.dispersion);
-	printf("\nSeveral_threads\nAverage value: %f\nDispersion: %f\n", val.average, val.dispersion);
-	//int i = 0;
-	//for(; i < AMOUNT_OF_DATA; i++) printf("%d\n", data[i]);
+	val = calculate(number_of_threads, data);
+	printf("\nNo threads\nAverage value: %f\nDispersion: %f\nTime spent: %f\n", val_no_thr.average, val_no_thr.dispersion, val_no_thr.time_spent);
+	printf("\n%d threads\nAverage value: %f\nDispersion: %f\nTime spent: %f\n", number_of_threads, val.average, val.dispersion, val.time_spent);
 	free(data);
 	return 0;
 }
@@ -52,46 +54,51 @@ void create_data(int * array) {
 	}
 }
 
-struct mean_variance calculate(int number_of_threads, int *data) {
+struct return_data calculate(int number_of_threads, int *data) {
 	if(!number_of_threads) {
-		struct mean_variance value = {0, 0};
-		double sum = 0;
+		struct return_data value = {0, 0, 0};
+		double sum_disp = 0;
 		int i = 0;
+		clock_t begin = clock();
 		for( ; i < AMOUNT_OF_DATA; i++) {
-			sum += (double)data[i];
+			sum_disp += (double)data[i];
 		}
-		value.average = sum / AMOUNT_OF_DATA;
+		value.average = sum_disp / AMOUNT_OF_DATA;
+		sum_disp = 0;
+		for(i = 0; i < AMOUNT_OF_DATA; i++) {
+			sum_disp += (data[i] - value.average) * (data[i] - value.average);
+		}
+		value.dispersion = sum_disp / AMOUNT_OF_DATA;
+		clock_t end = clock();
+		value.time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
 		return value;
 	}
 	else {
 		int i = 0;
-		struct thread_info * inf = calloc(SEVERAL_THREADS, sizeof(struct thread_info));
-		struct mean_variance common = {0, 0};
-		inf[i].addend = AMOUNT_OF_DATA / SEVERAL_THREADS;
-		for(i = 1; i < SEVERAL_THREADS; i++) {
+		struct thread_info * inf = calloc(number_of_threads, sizeof(struct thread_info));
+		struct return_data common = {0, 0, 0};
+		inf[i].addend = AMOUNT_OF_DATA / number_of_threads;
+		for(i = 0; i < number_of_threads; i++) {
 			inf[i].addend = inf[0].addend;
-		}
-		for(i = 0; i < SEVERAL_THREADS; i++) {
 			inf[i].th_mark = i + 1;
-		}
-		for(i = 0; i < SEVERAL_THREADS; i++) {
 			inf[i].start = (data + i * inf[i].addend);
+			inf[i].values = calloc(1, sizeof(struct return_data));
 		}
-		for(i = 0; i < SEVERAL_THREADS; i++) {
-			inf[i].values = calloc(1, sizeof(struct mean_variance));
-		}
-		for(i = 0; i < SEVERAL_THREADS; i++) {
+		clock_t begin = clock();
+		for(i = 0; i < number_of_threads; i++) {
 			int err = pthread_create(&inf[i].th_mark, NULL, &thread_work, &inf[i]);
 			if(err != 0) perror("create");
 		}
-		for(i = 0; i < SEVERAL_THREADS; i++) {
+		for(i = 0; i < number_of_threads; i++) {
 			pthread_join(inf[i].th_mark, NULL);
-		}
-		for(i = 0; i < SEVERAL_THREADS; i++) {
 			common.average += inf[i].values->average;
+			common.dispersion += inf[i].values->dispersion;
 		}
-		common.average = common.average / SEVERAL_THREADS;
-		for(i = 0; i < SEVERAL_THREADS; i++) {
+		common.average = common.average / number_of_threads;
+		common.dispersion = common.dispersion / number_of_threads;
+		clock_t end = clock();
+		common.time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+		for(i = 0; i < number_of_threads; i++) {
 			free(inf[i].values);
 		}
 		free(inf);
@@ -102,10 +109,14 @@ struct mean_variance calculate(int number_of_threads, int *data) {
 void *thread_work(void *arg) {
 	int i = 0;
 	struct thread_info *tinfo = (struct thread_info*)arg;
-	for(;i < tinfo->addend; i++) {
+	for( ;i < tinfo->addend; i++) {
 		tinfo->values->average += tinfo->start[i];
 	}
 	tinfo->values->average = tinfo->values->average / tinfo->addend;
+	for(i = 0; i < tinfo->addend; i++) {
+		tinfo->values->dispersion += (tinfo->start[i] - tinfo->values->average) * (tinfo->start[i] - tinfo->values->average);
+	}
+	tinfo->values->dispersion = tinfo->values->dispersion / tinfo->addend;
 	return NULL;
 }
-	
+
