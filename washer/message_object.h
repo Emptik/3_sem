7ec0dc56709be323f34a0dfb88a8cbc022a5fa83
Vlad Korovin <KorovinVA@phystech.edu>
2018-11-17 23:24:28 +0300

    delete a very big fault

--- washer/message_object.h
+++ washer/message_object.h
@@ -1,16 +1,17 @@
+#include <cstring>
+
 class message {
 public:
 	long type;
-	char *dish;
+	char dish[10];
 	int fd;
 	message() {
 		type = 0; 
-		dish = 0;
 		fd = 0;
 	}
 	message(char *array) {
 		type = 1;
-		dish = array;
+		strcpy(dish, array);
 		fd = 0;
 	}
 	char *get_dish() { return dish; }
