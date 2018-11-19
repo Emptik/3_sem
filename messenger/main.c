bc27b5ef4bc4379c5981076c8b09bff769f89371
Vlad Korovin <KorovinVA@phystech.edu>
2018-11-19 22:42:11 +0300

    x ? a:b

--- messenger/main.c
+++ messenger/main.c
@@ -13,44 +13,40 @@
 #define MAX_MESSAGE_SIZE 100
 #define ARRAY_SIZE 1000
 
-void client_work(int *fd, char *input, char *output, int write_pid, int print_pid);
+void client_work(int *fd, char *input, char *output, int pid);
 
 int main(int argc, char *argv[], char *envp[]) {
 	int fd[2] = {0};
 	char input[ARRAY_SIZE] = {0};
 	char output[ARRAY_SIZE] = {0};
-	pid_t write_pid = 0;
-	pid_t print_pid = 0;
+	pid_t pid = 0;
 	mkfifo("1_client_input", 0777);
 	mkfifo("2_client_input", 0777);
 	
-	// FIXIT: кажется, что одного fork вполне достаточно. и как раз пропадут сложные логические выражения с !print_pid и !write_pid
+	// FIXIT: кажется, что одного fork вполне достаточно. и как раз пропадут сложные логические выражения с !print_pid и !pid
 	// Еще теоретически для сокращения объема кода может быть удобен тернарный оператор условие ? выражение1 : выражение2
-	write_pid = fork();
-	if(write_pid) print_pid = fork();
+	pid = fork();
 	if(atoi(argv[1]) == FIRST_CLIENT) {
-		if(!write_pid) fd[1] = open("1_client_input", O_WRONLY);
-		if(!print_pid && write_pid) fd[0] = open("2_client_input", O_RDONLY);
+		(!pid) ? (fd[1] = open("1_client_input", O_WRONLY)) : (fd[0] = open("2_client_input", O_RDONLY));
 		while(1) {
-			client_work(fd, input, output, write_pid, print_pid);
+			client_work(fd, input, output, pid);
 		}
 	}
 	if(atoi(argv[1]) == SECOND_CLIENT) {
-		if(!write_pid) fd[1] = open("2_client_input", O_WRONLY);
-		if(!print_pid && write_pid) fd[0] = open("1_client_input", O_RDONLY);
+		(!pid) ? (fd[1] = open("2_client_input", O_WRONLY)) : (fd[0] = open("1_client_input", O_RDONLY));
 		while(1) {
-			client_work(fd, input, output, write_pid, print_pid);
+			client_work(fd, input, output, pid);
 		}
 	}
 	return 0;
 }
 
-void client_work(int *fd, char *input, char *output, int write_pid, int print_pid) {
-	if(!write_pid && !print_pid) {
+void client_work(int *fd, char *input, char *output, int pid) {
+	if(!pid) {
 		fgets(input, MAX_MESSAGE_SIZE, stdin);
 		write(fd[1], input, MAX_MESSAGE_SIZE);
 	}
-	if(!print_pid && write_pid) {
+	else {
 		read(fd[0], output, MAX_MESSAGE_SIZE);
 		fprintf(stdout, "%s", output);
 	}
